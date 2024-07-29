cc81be30fe5051452c43636d81319e2f1d01faee
arbiterkdh <arbiterkdh@gmail.com>
2024-07-29 15:11:24 +0900

    첫 커밋

---
+++ one-week-project1-backend/src/main/java/com/oneweekproject1backend/OneWeekProject1BackendApplication.java
@@ -0,0 +1,13 @@
+package com.oneweekproject1backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneWeekProject1BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OneWeekProject1BackendApplication.class, args);
+    }
+
+}
---
+++ one-week-project1-backend/src/test/java/com/oneweekproject1backend/OneWeekProject1BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oneweekproject1backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneWeekProject1BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
